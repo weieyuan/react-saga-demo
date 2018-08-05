218ad0d912edaff9e361b956009ff1998c152a33
危鄂元 <weieyuan@yeah.net>
2018-08-05 22:45:25 +0800

    新增eslint的配置文件

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ]
+    }
+};
